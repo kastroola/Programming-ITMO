332603df6c1f7c8dbfd05e124928d3cbaedbf325
kastroola <61935521+kastroola@users.noreply.github.com>
2021-12-11 15:10:03 +0300

    lab-1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Enter string\n");
+    int c;
+    while ((c = getchar()) != '\n')
+            prinf("%c", c);
+
+    printf("\nEnter float num\n");
+    foat f;
+    scanf("%f", &f);
+    printf("%f\n", f);
+
+    return 0
+}
